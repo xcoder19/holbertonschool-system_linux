5f791a98ad89eaf547e97afb5d4e70213b65d5ed
xcoder19 <hedihamida096@gmail.com>
2024-06-08 21:57:37 +0100

    betty

--- ls/hls.c
+++ ls/hls.c
@@ -1,24 +1,30 @@
 #include <stdio.h>
 #include <dirent.h>
 
-int main() {
-    struct dirent *entry;
-    DIR *dp = opendir(".");
+/**
+ * main - entry point
+ *
+ * Return: 1 if error and 0 if success
+ */
+int main(void)
+{
+	struct dirent *entry;
+	DIR *dp = opendir(".");
 
-    if (dp == NULL) {
-        perror("opendir");
-        return 1;
-    }
+	if (dp == NULL)
+	{
+		perror("opendir");
+		return (1);
+	}
 
-    while ((entry = readdir(dp))) {
-        if (entry->d_name[0] != '.') {
-            printf("%s  ", entry->d_name);
-        }
-    }
+	while ((entry = readdir(dp)))
+	{
+		if (entry->d_name[0] != '.')
+		printf("%s  ", entry->d_name);
+	}
 
-    closedir(dp);
-    printf("\n");
+	closedir(dp);
+	printf("\n");
 
-    return 0;
+	return (0);
 }
-
