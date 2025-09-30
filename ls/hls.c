18715945bde80ec249e64e2245032b06bc5be2ee
xcoder19 <hedihamida98@gmail.com>
2025-09-30 15:26:08 +0100

    fix the way it prints directories and files

--- ls/hls.c
+++ ls/hls.c
@@ -15,10 +15,9 @@ int iterate(DIR *directory, struct dirent *entry)
 	while ((entry = readdir(directory)) != NULL)
 	{
 		if (entry->d_name[0] != '.')
-		{
 			printf("%s ", entry->d_name);
-		}
 	}
+	putchar('\n');
 	return (0);
 }
 /**
@@ -36,32 +35,34 @@ int main(int argc, char *argv[])
 	{
 		for (int i = 1; i < argc; i++)
 		{
+			if (argc >= 3)
+				printf("%s:\n", argv[i]);
+
 			directory = opendir(argv[i]);
 			if (directory == NULL)
 			{
+				fprintf(stderr, "%s: %s %s: %s", argv[0], "cannot acess",
+						argv[i], "No such a file or directory");
 				return (1);
 			}
 
 			iterate(directory, entry);
 
 			if (closedir(directory) == -1)
-			{
 				return (1);
-			}
 		}
 	}
 	else
 	{
 		directory = opendir(".");
+
 		if (directory == NULL)
-		{
 			return (1);
-		}
+
 		iterate(directory, entry);
+
 		if (closedir(directory) == -1)
-		{
 			return (1);
-		}
 	}
 	return (0);
 }
