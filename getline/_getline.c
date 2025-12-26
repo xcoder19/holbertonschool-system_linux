b7a9022182b9625a62d0a5832ea7fed3b9ae2370
xcoder19 <hedihamida98@gmail.com>
2025-12-26 20:06:50 +0100

    support multiple fd using linked list

--- getline/_getline.c
+++ getline/_getline.c
@@ -1,80 +1,114 @@
 #include "_getline.h"
 
 /**
- * safe_free - frees memory and sets pointer to NULL
- * @ptr: pointer to pointer to free
+ * free_all_states - frees all fd states in the linked list
+ * @head: pointer to head pointer of list
  */
-void safe_free(char **ptr)
+void free_all_states(fd_state_t **head)
 {
-	free(*ptr);
-	*ptr = NULL;
+	fd_state_t *current = *head;
+	fd_state_t *next;
+
+	while (current != NULL)
+	{
+		next = current->next;
+		free(current);
+		current = next;
+	}
+	*head = NULL;
 }
 
 /**
- * handle_eof - handles end of file or error from read
- * @bytes_read: return value from read()
- * @line: pointer to line buffer
- * @i: number of characters in line
+ * get_fd_state - finds or creates state for a file descriptor
+ * @head: pointer to head pointer of list
+ * @fd: file descriptor to find
  *
- * Return: line if partial data exists, NULL otherwise
+ * Return: pointer to fd_state, or NULL on failure
  */
-char *handle_eof(ssize_t bytes_read, char **line, int i)
+fd_state_t *get_fd_state(fd_state_t **head, int fd)
 {
-	if (bytes_read == 0)
+	fd_state_t *current = *head;
+	fd_state_t *new_state;
+
+	while (current != NULL)
 	{
-		if (i == 0)
+		if (current->fd == fd)
+			return (current);
+		current = current->next;
+	}
+	new_state = malloc(sizeof(fd_state_t));
+	if (new_state == NULL)
+		return (NULL);
+	new_state->fd		  = fd;
+	new_state->buffer_pos = 0;
+	new_state->buffer_len = 0;
+	new_state->next		  = *head;
+	*head				  = new_state;
+	return (new_state);
+}
+
+/**
+ * read_line - reads a line from fd using given state
+ * @state: fd state structure
+ * @line: buffer to store line
+ *
+ * Return: line on success, NULL on EOF/error
+ */
+char *read_line(fd_state_t *state, char *line)
+{
+	int		i = 0;
+	ssize_t bytes_read;
+
+	while (1)
+	{
+		while (state->buffer_pos < state->buffer_len)
 		{
-			safe_free(line);
+			if (state->buffer[state->buffer_pos] == '\n')
+			{
+				line[i] = '\0';
+				state->buffer_pos++;
+				return (line);
+			}
+			line[i++] = state->buffer[state->buffer_pos++];
+		}
+		bytes_read = read(state->fd, state->buffer, READ_SIZE);
+		if (bytes_read <= 0)
+		{
+			if (bytes_read == 0 && i > 0)
+			{
+				line[i] = '\0';
+				return (line);
+			}
+			free(line);
 			return (NULL);
 		}
-		(*line)[i] = '\0';
-		return (*line);
+		state->buffer_len = bytes_read;
+		state->buffer_pos = 0;
 	}
-	if (bytes_read == -1)
-	{
-		safe_free(line);
-		return (NULL);
-	}
-	return (NULL);
 }
 
 /**
  * _getline - reads an entire line from a file descriptor
- * @fd: file descriptor to read from
+ * @fd: file descriptor to read from, -1 to free all and reset
  *
  * Return: null-terminated string without newline, or NULL on error/EOF
  */
 char *_getline(const int fd)
 {
-	static char	   buffer[READ_SIZE];
-	static ssize_t buffer_pos;
-	static ssize_t buffer_len;
-	int			   i	= 0;
-	char		  *line = malloc(READ_SIZE);
-	ssize_t		   bytes_read;
-	char		  *result;
+	static fd_state_t *head;
+	fd_state_t		  *state;
+	char			  *line;
 
+	if (fd == -1)
+	{
+		free_all_states(&head);
+		return (NULL);
+	}
+	state = get_fd_state(&head, fd);
+	if (state == NULL)
+		return (NULL);
+	line = malloc(READ_SIZE);
 	if (line == NULL)
 		return (NULL);
-	while (1)
-	{
-		while (buffer_pos < buffer_len)
-		{
-			if (buffer[buffer_pos] == '\n')
-			{
-				line[i] = '\0';
-				buffer_pos++;
-				return (line);
-			}
-			line[i++] = buffer[buffer_pos++];
-		}
-		bytes_read = read(fd, buffer, READ_SIZE);
-		if (bytes_read <= 0)
-		{
-			result = handle_eof(bytes_read, &line, i);
-			return (result);
-		}
-		buffer_len = bytes_read;
-		buffer_pos = 0;
-	}
+	return (read_line(state, line));
 }
--- getline/_getline.h
+++ getline/_getline.h
@@ -7,6 +7,23 @@
 
 #define READ_SIZE 1024
 
+/**
+ * struct fd_state_s - state for a file descriptor
+ * @fd: file descriptor
+ * @buffer: read buffer
+ * @buffer_pos: current position in buffer
+ * @buffer_len: bytes currently in buffer
+ * @next: pointer to next fd_state
+ */
+typedef struct fd_state_s
+{
+	int				   fd;
+	char			   buffer[READ_SIZE];
+	ssize_t			   buffer_pos;
+	ssize_t			   buffer_len;
+	struct fd_state_s *next;
+} fd_state_t;
+
 char *_getline(const int fd);
 
 #endif /* _GETLINE_H */
